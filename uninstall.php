580c577f8bf92016b6f02c937a520fb9588891da
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-12-19 21:24:15 +0300

    Empty file

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Unistall Social Network Widget.
+ * Fired when the plugin is uninstalled.
+ *
+ * @author      Mahdi Yazdani
+ * @package     Social Network
+ * @since       1.0.0
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+} // End If Statement
